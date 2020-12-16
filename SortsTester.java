1fdbdfdca99b2aa089f8d9e1f23df072cf2cff22
tinalenguyen <>
2020-12-16 00:33:16 -0500

    tested one case will add more

---
+++ SortsTester.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SortsTester{
+
+    public static void main(String[] args){
+      int[] tests = new int[]{6, 1, 2, 3, 4, 5};
+      Sorts.bubbleSort(tests);
+      System.out.println(Arrays.toString(tests));
+
+    }
+
+
+}
