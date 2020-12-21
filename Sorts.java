b9504fda72bd14841748294375c3085d75f14aaa
tinalenguyen <>
2020-12-21 02:50:34 -0500

    works better i suppose

--- Sorts.java
+++ Sorts.java
@@ -34,33 +34,30 @@ public class Sorts{
   }
 
   public static void insertionSort(int[] data){
-
-
     int saver = 0;
-    int ind = 0 ;
-    for (int i = 1 ; i < data.length-1 ; i++){
+
+    for (int i = 1 ; i < data.length ; i++){
+
       if (data[i] < data[i-1]) {
-      for (int j = 0 ; j < i ; j++){
-          if (data[i] < data[0]){
-            saver = data[0];
-             data[0] = data[i];
-           }
-          if (data[i] < data[j] && data[i] > data[j-1] && j >= 1 ){
-            saver = data[j];
-            ind = j;
+
+      for (int j = i-1 ; j > 0 ; j--){
+
+          if (data[i] < data[j]){
+            if ((j >= 1 && data[i] > data[j-1])||(j == 0)){
+
             data[j] = data[i];
-          }
 
+            }
+          }
           else data[j+1] = data[j];
-
-          }
+        }
       }
 
     }
 
 
 
-  }
+ }
 
 
 }
